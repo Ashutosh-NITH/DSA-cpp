8edd59b79f4945cc8e4bbf537f363d4cae6be494
Ashutosh-NITH <ashutosh.93343.kumar@gmail.com>
2025-07-27 14:37:32 +0530

    linked list implementation

---
+++ Linked List/implementation.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data ;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+};
+
+class Linked_List {
+    Node* head;
+    Node* tail;
+public: 
+    Linked_List(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+
+        Node* newNode = new Node(val); // new keyword gives instance(address) of the object created which what lhs wants
+        if(head == NULL){
+            head = tail = newNode;
+        }else{
+            (*newNode).next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(tail == NULL){
+            head = tail = newNode;
+        }else{
+            (*tail).next = newNode;
+            tail = newNode;
+
+        }
+    }
+
+    void print_linked_list(){
+        Node* temp = head;
+
+        while(temp != NULL){
+            cout<<(*temp).data<<" -> ";
+            temp = (*temp).next; 
+        }
+        cout<<"NULL"<<endl;
+    }
+
+    void pop_front(){
+
+        if(head == NULL){
+            return;
+        }
+        Node* temp = head;
+        head = (*head).next;
+        delete temp;
+    }
+
+    void  pop_back(){
+        if(tail == NULL){
+            return;
+        }else{
+            Node* temp = head;
+            while( (*((*temp).next)).next != NULL ){
+                temp = (*temp).next;
+            }
+            (*temp).next = NULL;
+            delete tail;
+            tail = temp;
+        }
+    }
+
+    void insert_at(int val , int pos){
+        if(pos<0){
+            return ;
+        }else if(pos == 0){
+            push_front(val);
+        }else{
+            Node* newNode = new Node(val);
+
+            Node* temp = head;
+
+            for(int i=0 ; i<pos-1; i++){
+                temp = (*temp).next ;
+            }
+            (*newNode).next = (*temp).next;
+            (*temp).next = newNode;
+        }
+    }
+};
+
+
+int main(){
+    Linked_List ll;
+
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+    ll.insert_at(7,1);
+
+    ll.print_linked_list();
+
+return 0;
+}
