b3b45bd5c50080b5c0b6c0c17e9579b398b0300c
Ashutosh-NITH <ashutosh.93343.kumar@gmail.com>
2025-07-27 15:01:54 +0530

    linked list all functions

--- Linked List/implementation.cpp
+++ Linked List/implementation.cpp
@@ -21,7 +21,7 @@ public:
         head = tail = NULL;
     }
 
-    void push_front(int val){
+    void push_front(int val){  //O(1)
 
         Node* newNode = new Node(val); // new keyword gives instance(address) of the object created which what lhs wants
         if(head == NULL){
@@ -32,7 +32,7 @@ public:
         }
     }
 
-    void push_back(int val){
+    void push_back(int val){ //O(1)
         Node* newNode = new Node(val);
 
         if(tail == NULL){
@@ -44,7 +44,7 @@ public:
         }
     }
 
-    void print_linked_list(){
+    void print_linked_list(){ //O(n)
         Node* temp = head;
 
         while(temp != NULL){
@@ -54,7 +54,7 @@ public:
         cout<<"NULL"<<endl;
     }
 
-    void pop_front(){
+    void pop_front(){ //O(1)
 
         if(head == NULL){
             return;
@@ -64,7 +64,7 @@ public:
         delete temp;
     }
 
-    void  pop_back(){
+    void  pop_back(){   //O(n)
         if(tail == NULL){
             return;
         }else{
@@ -78,8 +78,9 @@ public:
         }
     }
 
-    void insert_at(int val , int pos){
+    void insert_at(int val , int pos){  //O(n)
         if(pos<0){
+            cout<<"invalid postion"<<endl;
             return ;
         }else if(pos == 0){
             push_front(val);
@@ -89,11 +90,34 @@ public:
             Node* temp = head;
 
             for(int i=0 ; i<pos-1; i++){
+                if(temp == NULL){
+                    cout<<"invalid position"<<endl;
+                    return ;
+                }
                 temp = (*temp).next ;
             }
             (*newNode).next = (*temp).next;
             (*temp).next = newNode;
         }
+    }
+
+
+
+    int search (int target){ //O(n)
+        Node* temp = head ;
+        int index = 0 ;
+
+        while(temp !=NULL){
+            if((*temp).data == target ){
+                return index;
+            }
+            index ++;
+            temp = (*temp).next;
+           
+        }
+        return index;
+
+        return -1;
     }
 };
 
@@ -106,8 +130,9 @@ int main(){
     ll.push_front(3);
     ll.push_front(4);
     ll.insert_at(7,1);
-
+    ll.insert_at(3,50);
     ll.print_linked_list();
+    cout<<ll.search(7);
 
 return 0;
 }
